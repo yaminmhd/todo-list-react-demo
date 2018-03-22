e33f3ff217e772c025bc43b231bd80212ce13edb
yaminmhd <mdyamin.drzee@gmail.com>
2018-03-22 15:14:45 +0800

    Added test to TodoForm component

---
+++ src/components/todo-form/TodoForm.test.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import {shallow} from 'enzyme';
+import TodoList from '../todo-list/TodoList';
+import TodoTitle from '../todo-title/TodoTitle';
+import TodoForm from '../todo-form/TodoForm';
+import TodoItem from '../todo-item/TodoItem';
+
+
+describe('TodoForm', ()=> {
+  it('should render TodoForm properly', () => {
+    const testTodo = {description: 'buy lunch', isCompleted: false};
+    const wrapper = shallow(<TodoForm newTodos={testTodo}/>);
+    expect(wrapper.find('input')).toHaveLength(2);
+  });
+
+  it("should call handle Submit function supplied via props", () => {
+    const testTodo = {description: 'trial run', isCompleted: false};
+    const mockHandler = jest.fn();
+
+    const wrapper = shallow(<TodoForm newTodos={testTodo} handleSubmit={mockHandler} />);
+
+    wrapper.find("form").simulate("submit", { preventDefault() {} });
+
+    expect(mockHandler).toBeCalled();
+  });
+})
